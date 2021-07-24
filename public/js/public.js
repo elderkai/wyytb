3b6192ad8da53483cfc834d41bf04699e44e35b2
elderkai <2498695596@qq.com>
2021-07-24 16:09:17 +0800

    first commit

--- public/js/public.js
+++ public/js/public.js
@@ -1,23 +1,17 @@
-document.body.style.height = window.innerHeight + "px";
-/*动态改变根元素字体大小*/
-function recalc() {
-	var clientWidth = document.documentElement.clientWidth;
-	if(!clientWidth) return;
-	document.documentElement.style.fontSize = 40 * (clientWidth / 750) + 'px';
-								// 字体大小   = 1个rem单位表示多少个像素（设备的宽度   /设计宽度）
-}
-function initRecalc() {
-	var clientWidth = document.documentElement.clientWidth;
-	recalc();
-	var resizeEvt = 'osrientationchange' in window ? 'orientationchange' : 'resize';
-	if(!document.addEventListener) return;
-	window.addEventListener(resizeEvt, recalc, false);
-	document.addEventListener('DOMContentLoaded', recalc, false);
-	if(window.getComputedStyle(document.getElementsByTagName("html")[0]).fontSize) {
-		var size = window.getComputedStyle(document.getElementsByTagName("html")[0]).fontSize.split('p')[0];
-		if(size*1.2 < 40 * (clientWidth / 750)) {
-		   docEl.style.fontSize = 50 * (clientWidth / 750) + 'px';
-		}
-	  }
-}
-initRecalc();	
+(function(doc, win) {
+	var docEl = doc.documentElement,
+			resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+			recalc = function() {
+					var clientWidth = docEl.clientWidth;
+					if (!clientWidth) return;
+					if (clientWidth >= 750) {
+							docEl.style.fontSize = '100px';
+					} else {
+							docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+					}
+			};
+
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);	
